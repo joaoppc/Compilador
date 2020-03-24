5ccab8eaa5dfab27e839a4390f2e0cac9ef79522
joaoppc <jppc9292@gmail.com>
2020-03-24 20:32:28 -0300

    roteiro1

--- roteiro0.py
+++ roteiro0.py
@@ -1,55 +1,71 @@
 import sys
+class Token:
+    def __init__(self,type,value):
+        self.type = type
+        self.value = value
+class Tokenizer:
+    def __init__(self,origin):
+        self.origin = origin
+        self.position = 0
+        self.actual = 0
+        self.selectNext()
+    def selectNext(self):
+        while (self.position < len(self.origin)) and (self.origin[self.position] == " "):
+            self.position += 1
+        if self.position <len(self.origin):
+            self.actual = self.origin[self.position]
+            if self.origin[self.position] == '+':
+                self.actual = Token('PLUS','+')
+                self.position += 1
+            elif self.origin[self.position] == '-':
+                self.actual = Token('MINUS','-')
+                self.position += 1
+            elif self.origin[self.position].isdigit():
+                num = ""
+                
+                num += self.origin[self.position]
+                self.actual = Token('INT',int(num))
+                self.position += 1
+            
+class Parser:
+    tokens = None
+    @staticmethod
+    def run(code):
+        Parser.tokens = Tokenizer(code)
+        print(Parser.parseExpression())
+        
 
-op = ['+','-','*','/']
-white_space = ' '
-lexeme = ''
-number1 = ''
-aspa ="'"
-numlist = []
-opseq=[]
-for param  in sys.argv[1:] :
-    for i, char in enumerate(param):
-        if char != white_space: 
-            lexeme += char
-            if char in op or char == aspa:
-                if char == aspa:
-                    pass
-                else:
-                    opseq.append(char)
-                number1 = lexeme[:-1]
-                lexeme =''
-                numlist.append(number1)
-    
-numlist = numlist[1:]
-numlist.reverse()
-opseq.reverse()
-while len(opseq)> 0:
-    #print(len(numlist))
-    operand = opseq.pop()
-    if operand == '+':
-        num1 = numlist.pop()
-        num2 = numlist.pop()
-        #print(num1)
-        #print(num2)
-        result = int(num1)+int(num2)
-        #print(result)
-        numlist.append(result)
-    elif operand == '-':
-        num1 = numlist.pop()
-        num2 = numlist.pop()
-        #print(num1)
-        #print(num2)
-        result = int(num1)-int(num2)
-        #print(result)
-        numlist.append(result)
-    elif operand == '/':
-        result = int(numlist.pop())/int(numlist.pop())
-        #print(result)
-        numlist.append(result)
-    elif operand == '*':
-        result = int(numlist.pop())*int(numlist.pop())
-        #print(result)
-        numlist.append(result)
-print(result)
+    @staticmethod
+    def parseExpression():
+        result = ""
+        if Parser.tokens.actual.type == 'INT':
+            result = Parser.tokens.actual.value
+            Parser.tokens.selectNext()
+            while Parser.tokens.actual.type in ['PLUS','MINUS']:
 
+                if Parser.tokens.actual.type == "PLUS":
+                    Parser.tokens.selectNext()
+                    if Parser.tokens.actual.type == 'INT':
+                        result += Parser.tokens.actual.value
+                        Parser.tokens.selectNext()
+                    else:
+                        print('erro')
+                    
+                elif Parser.tokens.actual.type == "MINUS":
+                    Parser.tokens.selectNext()
+                    if Parser.tokens.actual.type == 'INT':
+                        result -= Parser.tokens.actual.value
+                        Parser.tokens.selectNext()
+                    else:
+                        print('erro') 
+            return result
+        else:
+            print('erro') 
+
+        
+
+
+if __name__ == '__main__':
+    code = sys.argv[1]
+    Parser.run(code)
  
